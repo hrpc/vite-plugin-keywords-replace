98bd38cd877b996185a4204ceb9429fa47a91f53
unknown <haorun@hikvision.com>
2021-04-29 18:01:13 +0800

    v3

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/vite-plugin-keyword-replace.js',
+    file: 'dist/vite-plugin-keywords-replace.js',
     format: 'cjs'
   }
 }
